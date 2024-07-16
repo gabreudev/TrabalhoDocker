28488e6564604e9b3985a5285086243071d4d29d
gabreudev <biel0alves0@gmail.com>
2024-07-16 20:44:26 -0300

    Initial commit

---
+++ src/main/java/com/trabalhoSO/TrabalhoDocker/TrabalhoDockerApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhoSO.TrabalhoDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabalhoSO/TrabalhoDocker/TrabalhoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalhoSO.TrabalhoDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
